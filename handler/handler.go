8261ec56547fda420a1c7c1f8811201fd7134f20
isaac <isaac@isaac-U43SD.(none)>
2013-12-26 18:26:07 +0300

    initial commit

---
+++ handler/handler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"appengine"
+	"appengine/user"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u, err := user.CurrentOAuth(c, "")
+	if err != nil {
+		http.Error(w, "OAuth Authorization header required", http.StatusUnauthorized)
+		return
+	}
+	if !u.Admin {
+		http.Error(w, "Admin login only", http.StatusUnauthorized)
+		return
+	}
+	fmt.Fprintf(w, `Welcome, admin user %s!`, u)
+
+}
